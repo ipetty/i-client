bca2c679bc187a9fd4eb42495bbd845dc96aa2fc
kongchun <kcrens@gmail.com>
2014-06-09 10:34:26 +0800

    登录注销后的用户登录界面

--- src/net/ipetty/android/login/LoginHasAccountActivity.java
+++ src/net/ipetty/android/login/LoginHasAccountActivity.java
@@ -4,8 +4,14 @@ import net.ipetty.R;
 import net.ipetty.android.core.ui.BackClickListener;
 import net.ipetty.android.core.ui.BaseActivity;
 import net.ipetty.android.core.util.AppUtils;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.Selection;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +20,10 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class LoginHasAccountActivity extends BaseActivity {
 	DisplayImageOptions options;
+
+	private EditText passwordView;
+	private TextView toggleView = null;
+	private boolean psdDisplayFlg = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,18 @@ public class LoginHasAccountActivity extends BaseActivity {
 		text.setText(this.getResources().getString(R.string.title_activity_login_has_account));
 		btnBack.setOnClickListener(new BackClickListener(this));
 
+		TextView changeAccount = (TextView) this.findViewById(R.id.changeAccount);
+		changeAccount.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(LoginHasAccountActivity.this, LoginActivity.class);
+				LoginHasAccountActivity.this.startActivity(intent);
+				// LoginHasAccountActivity.this.finish();
+			}
+		});
+
 		String textUrl = "http://weibo.kedacom.com/weibo/files/h/b9c31599803e48f0a0595e2e913714e4/h64.jpg?t=1388814731997";
 
 		options = AppUtils.getRoundedImageOptions();
@@ -33,14 +55,33 @@ public class LoginHasAccountActivity extends BaseActivity {
 		// avator
 		ImageView avator = (ImageView) this.findViewById(R.id.avator);
 		ImageLoader.getInstance().displayImage(textUrl, avator, options);
+		// passworid
+		passwordView = (EditText) this.findViewById(R.id.password);
+		toggleView = (TextView) this.findViewById(R.id.login_toggle_password);
+		toggleView.setOnClickListener(togglePasswordClick);
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login_has_account, menu);
-		return true;
-	}
+	// 密码可见
+	private OnClickListener togglePasswordClick = new OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			int index = passwordView.getSelectionStart();
+			if (!psdDisplayFlg) {
+				// display password text, for example "123456"
+				// passwordView.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+				passwordView.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+				toggleView.setText(R.string.login_toggle_password_hide);
+			} else {
+				// hide password, display "."
+				// passwordView.setTransformationMethod(PasswordTransformationMethod.getInstance());
+				passwordView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				toggleView.setText(R.string.login_toggle_password_show);
+			}
+			psdDisplayFlg = !psdDisplayFlg;
+			Editable etable = passwordView.getText();
+			Selection.setSelection(etable, index);
+		}
+	};
 
 }
