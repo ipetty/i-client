bf97273c3493f3f61bd85a1b5c92715092bc343c
kongchun <kcrens@gmail.com>
2014-06-18 15:17:41 +0800

    动态初步界面

--- src/net/ipetty/android/news/MainNewsFragment.java
+++ src/net/ipetty/android/news/MainNewsFragment.java
@@ -11,10 +11,22 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ViewFlipper;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
 public class MainNewsFragment extends Fragment {
 	public final static String TAG = "MainNewsFragment";
 	private Activity activity;
 	private ViewFlipper viewFlipper;
+
+	private View related_me;
+	private View my_follows;
+
+	private PullToRefreshListView related_me_listView;
+	private RelatedMeAdapter related_me_adapter;
+
+	private PullToRefreshListView my_follows_listView;
+	private MyFollowsAdapter my_follows_adapter;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,10 +48,21 @@ public class MainNewsFragment extends Fragment {
 		viewFlipper = (ViewFlipper) activity.findViewById(R.id.viewFlipper);
 		viewFlipper.setDisplayedChild(0);
 
-		View related_me = activity.findViewById(R.id.related_me);
-		View my_follows = activity.findViewById(R.id.my_follows);
+		related_me = activity.findViewById(R.id.related_me);
+		my_follows = activity.findViewById(R.id.my_follows);
 		related_me.setOnClickListener(new TabClickListener(0));
 		my_follows.setOnClickListener(new TabClickListener(1));
+
+		related_me_listView = (PullToRefreshListView) activity.findViewById(R.id.related_me_listView);
+		related_me_listView.setMode(Mode.PULL_FROM_END);
+
+		related_me_adapter = new RelatedMeAdapter(this.getActivity());
+		related_me_listView.setAdapter(related_me_adapter);
+
+		my_follows_listView = (PullToRefreshListView) activity.findViewById(R.id.my_follows_listView);
+		my_follows_listView.setMode(Mode.PULL_FROM_END);
+		my_follows_adapter = new MyFollowsAdapter(this.getActivity());
+		my_follows_listView.setAdapter(my_follows_adapter);
 	}
 
 	public class TabClickListener implements OnClickListener {
@@ -53,8 +76,14 @@ public class MainNewsFragment extends Fragment {
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			viewFlipper.setDisplayedChild(index);
+			if (index == 0) {
+				related_me.setBackgroundResource(R.drawable.news_tab_selected);
+				my_follows.setBackgroundResource(R.drawable.trans);
+			} else {
+				related_me.setBackgroundResource(R.drawable.trans);
+				my_follows.setBackgroundResource(R.drawable.news_tab_selected);
+			}
 		}
-
 	}
 
 	@Override
---
+++ src/net/ipetty/android/news/MyFollowsAdapter.java
@@ -0,0 +1,99 @@
+package net.ipetty.android.news;
+
+import java.util.List;
+
+import net.ipetty.R;
+import net.ipetty.android.demo.CommentVO;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyFollowsAdapter extends BaseAdapter implements OnScrollListener {
+	public final static String TAG = "MyFollowsAdapter";
+	private LayoutInflater inflater;
+	private List<CommentVO> list = null; // 这个就本地dataStore
+
+	public MyFollowsAdapter(Context context) {
+		// TODO Auto-generated constructor stub
+		this.inflater = LayoutInflater.from(context);
+	}
+
+	@Override
+	public int getCount() {
+		return 2;// list == null ? 0 : list.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return 0;// list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;// list.get(position).getId();
+	}
+
+	// 构建一个布局缓存的结构体 与VO对应
+	// 构建一个布局缓存的结构体 与VO对应
+	public class ViewHolder {
+		public ImageView avatar;
+		public TextView name;
+		public ImageView follow;
+	}
+
+	public ViewHolder holder;
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		Log.i(TAG, "list position-->" + position);
+		// 这里开始呈现每个item的布局
+		View view;
+		if (convertView == null) {
+			Log.i(TAG, "init items View");
+			view = inflater.inflate(R.layout.list_my_follows_item, null);
+			holder = new ViewHolder();
+			holder.avatar = (ImageView) view.findViewById(R.id.avatar);
+			holder.name = (TextView) view.findViewById(R.id.name);
+
+			convertView = view;
+			convertView.setTag(holder);
+		} else {
+			view = convertView;
+			holder = (ViewHolder) view.getTag();
+		}
+		// 数据与界面绑定
+
+		return view;
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<CommentVO> getList() {
+		return list;
+	}
+
+	public void setList(List<CommentVO> list) {
+		this.list = list;
+	}
+
+}
