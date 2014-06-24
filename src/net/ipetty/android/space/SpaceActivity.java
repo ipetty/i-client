5e894ba3baddbaed24ab6ee125e8fc479e368435
jinghai <jinghai.xiao@gmail.com>
2014-06-24 17:53:13 +0800

    个人空间（未测试）

--- src/net/ipetty/android/space/SpaceActivity.java
+++ src/net/ipetty/android/space/SpaceActivity.java
@@ -14,7 +14,9 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import net.ipetty.R;
 import net.ipetty.android.api.UserApiWithCache;
+import net.ipetty.android.bonuspoint.BonusPointActivity;
 import net.ipetty.android.core.Constant;
+import net.ipetty.android.core.DefaultTaskListener;
 import net.ipetty.android.core.ui.BackClickListener;
 import net.ipetty.android.core.util.AppUtils;
 import net.ipetty.android.discover.DiscoverAdapter;
@@ -22,20 +24,30 @@ import net.ipetty.android.fans.FansActivity;
 import net.ipetty.android.follow.FollowsActivity;
 import net.ipetty.android.petty.PettyActivity;
 import net.ipetty.android.sdk.core.IpetApi;
+import net.ipetty.android.sdk.task.user.GetUserStatisticsByUserId;
+import net.ipetty.android.sdk.task.user.IsFollow;
 import net.ipetty.android.user.UserActivity;
+import net.ipetty.vo.UserStatisticsVO;
 import net.ipetty.vo.UserVO;
 import org.apache.commons.lang3.StringUtils;
 
 public class SpaceActivity extends Activity {
 
-    public final static String TAG = "SpaceActivity";
+    public final static String TAG = SpaceActivity.class.getSimpleName();
+    private Integer userId;
+    private Integer currUserId;
+    private Boolean isCurrentUser;
     private ViewFlipper viewFlipper;
     private View space_petty_view;
     private GridView space_photo_grid;
     private DiscoverAdapter space_photo_grid_adapter;
-    private Integer userId;
-    private Integer currUserId;
+
     private DisplayImageOptions options = AppUtils.getNormalImageOptions();
+    private TextView feed_num_text;
+    private TextView follow_num_text;
+    private TextView fan_num_text;
+    private TextView action_bar_right_text;//关注/修改个人信息
+    private View follows;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,7 @@ public class SpaceActivity extends Activity {
 
         userId = this.getIntent().getExtras().getInt("id");
         currUserId = IpetApi.init(this).getCurrUserId();
+        isCurrentUser = userId == currUserId;
 
         /* action bar */
         ImageView btnBack = (ImageView) this.findViewById(R.id.action_bar_left_image);
@@ -53,7 +66,7 @@ public class SpaceActivity extends Activity {
 
         String title = this.getResources().getString(R.string.title_activity_space);
         // 标题
-        if (!isCurrentUser()) {
+        if (!isCurrentUser) {
             String title_space = getResources().getString(R.string.title_space);
             String username = user.getNickname();
             title = String.format(title_space, username);
@@ -61,26 +74,65 @@ public class SpaceActivity extends Activity {
         TextView text = (TextView) this.findViewById(R.id.action_bar_title);
         text.setText(title);
         // 右侧操作按钮
-        TextView action_bar_right_text = (TextView) this.findViewById(R.id.action_bar_right_text);
-        if (isCurrentUser()) {
+        action_bar_right_text = (TextView) this.findViewById(R.id.action_bar_right_text);
+        if (isCurrentUser) {
             action_bar_right_text.setText(R.string.user_edit);
             action_bar_right_text.setOnClickListener(userEditClick);
         } else {
-            // TODO:判断用户是否关注
-            if (true) {
-                action_bar_right_text.setText(R.string.follow_text);
-            } else {
-                action_bar_right_text.setText(R.string.unfollow_text);
-            }
-            action_bar_right_text.setOnClickListener(toggleFollowClick);
+            // 判断用户是否关注
+            new IsFollow(this)
+                    .setListener(new DefaultTaskListener<Boolean>(SpaceActivity.this) {
+                        @Override
+                        public void onSuccess(Boolean result) {
+                            if (result) {
+                                action_bar_right_text.setText(R.string.follow_text);
+                            } else {
+                                action_bar_right_text.setText(R.string.unfollow_text);
+                            }
+                            action_bar_right_text.setOnClickListener(toggleFollowClick);
+                        }
+                    })
+                    .execute(this.userId);
+
         }
+
+        //TODO:用户统计信息
+        feed_num_text = (TextView) this.findViewById(R.id.feed_num_text);
+        follow_num_text = (TextView) this.findViewById(R.id.follow_num_text);
+        fan_num_text = (TextView) this.findViewById(R.id.fan_num_text);
+
+        new IsFollow(this)
+                .setListener(new DefaultTaskListener<Boolean>(SpaceActivity.this) {
+                    @Override
+                    public void onSuccess(Boolean result) {
+                        if (result) {
+                            action_bar_right_text.setText(R.string.follow_text);
+                        } else {
+                            action_bar_right_text.setText(R.string.unfollow_text);
+                        }
+                        action_bar_right_text.setOnClickListener(toggleFollowClick);
+                    }
+                })
+                .execute(this.userId);
+
+        new GetUserStatisticsByUserId(this)
+                .setListener(new DefaultTaskListener<UserStatisticsVO>(SpaceActivity.this) {
+                    @Override
+                    public void onSuccess(UserStatisticsVO result) {
+                        feed_num_text.setText(result.getFeedNum());
+                        follow_num_text.setText(result.getFollowerNum());
+                        fan_num_text.setText(result.getFollowerNum());
+
+                    }
+                })
+                .execute(this.userId);
 
         // 头像
         ImageView avatar = (ImageView) this.findViewById(R.id.avatar);
         if (!StringUtils.isEmpty(user.getAvatar())) {
             ImageLoader.getInstance().displayImage(Constant.FILE_SERVER_BASE + user.getAvatar(), avatar, options);
         }
-        if (isCurrentUser()) {
+        if (isCurrentUser) {
             avatar.setOnClickListener(userEditClick);
         }
 
@@ -88,21 +140,28 @@ public class SpaceActivity extends Activity {
         fans.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 Intent intent = new Intent(SpaceActivity.this, FansActivity.class);
+                if (SpaceActivity.this.isCurrentUser) {
+                    intent.putExtra("id", SpaceActivity.this.currUserId);
+                } else {
+                    intent.putExtra("id", SpaceActivity.this.userId);
+                }
                 startActivity(intent);
-                // finish();
             }
         });
 
-        View follows = findViewById(R.id.follows_layout);
+        follows = findViewById(R.id.follows_layout);
+
         follows.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 Intent intent = new Intent(SpaceActivity.this, FollowsActivity.class);
+                if (SpaceActivity.this.isCurrentUser) {
+                    intent.putExtra("id", SpaceActivity.this.currUserId);
+                } else {
+                    intent.putExtra("id", SpaceActivity.this.userId);
+                }
                 startActivity(intent);
-                // finish();
             }
         });
 
@@ -110,10 +169,13 @@ public class SpaceActivity extends Activity {
         bonusPoint.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                //Intent intent = new Intent(SpaceActivity.this, BonusPointActivity.class);
-                //startActivity(intent);
-                // finish();
+                Intent intent = new Intent(SpaceActivity.this, BonusPointActivity.class);
+                if (SpaceActivity.this.isCurrentUser) {
+                    intent.putExtra("id", SpaceActivity.this.currUserId);
+                } else {
+                    intent.putExtra("id", SpaceActivity.this.userId);
+                }
+                startActivity(intent);
             }
         });
 
@@ -132,13 +194,13 @@ public class SpaceActivity extends Activity {
         space_petty_view = this.findViewById(R.id.list_space_petty_item);
         View pet_edit_view = space_petty_view.findViewById(R.id.pet_edit_view);
 
-        if (isCurrentUser()) {
+        if (isCurrentUser) {
             pet_edit_view.setVisibility(View.VISIBLE);
             pet_edit_view.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // TODO Auto-generated method stub
                     Intent intent = new Intent(SpaceActivity.this, PettyActivity.class);
+                    intent.putExtra("id", SpaceActivity.this.currUserId);
                     startActivity(intent);
                 }
             });
@@ -161,19 +223,15 @@ public class SpaceActivity extends Activity {
 
         @Override
         public void onClick(View v) {
-            // TODO Auto-generated method stub
             viewFlipper.setDisplayedChild(index);
         }
-    }
-
-    private boolean isCurrentUser() {
-        return currUserId == userId;
     }
 
     private OnClickListener userEditClick = new OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(SpaceActivity.this, UserActivity.class);
+            intent.putExtra("id", SpaceActivity.this.currUserId);
             startActivity(intent);
         }
     };
