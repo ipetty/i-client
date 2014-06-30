4fa39d972083420728721a02bd629ab4e4df34c6
jinghai <jinghai.xiao@gmail.com>
2014-06-30 13:52:33 +0800

    修复onCancelled定义问题

--- src/net/ipetty/android/core/DefaultTaskListener.java
+++ src/net/ipetty/android/core/DefaultTaskListener.java
@@ -23,158 +23,158 @@ import org.springframework.web.client.RestClientException;
  */
 public abstract class DefaultTaskListener<Result> implements TaskListener<Result> {
 
-    private final static String TAG = DefaultTaskListener.class.getSimpleName();
+	private final static String TAG = DefaultTaskListener.class.getSimpleName();
 
-    protected Activity activity;
+	protected Activity activity;
 
-    private String loadingMessage;
+	private String loadingMessage;
 
-    private ProgressDialog progressDialog;
+	private ProgressDialog progressDialog;
 
-    /**
-     * 带有默认loading信息的构造
-     *
-     * @param activity
-     */
-    public DefaultTaskListener(Activity activity) {
-        this.activity = activity;
-    }
+	/**
+	 * 带有默认loading信息的构造
+	 *
+	 * @param activity
+	 */
+	public DefaultTaskListener(Activity activity) {
+		this.activity = activity;
+	}
 
-    /**
-     * 带有自定义loading信息的构造
-     *
-     * @param activity
-     * @param loadingMessage
-     */
-    public DefaultTaskListener(Activity activity, String loadingMessage) {
-        this(activity);
-        this.loadingMessage = loadingMessage;
-        this.progressDialog = new ProgressDialog(this.activity);
-        this.progressDialog.setIndeterminate(true);
-    }
+	/**
+	 * 带有自定义loading信息的构造
+	 *
+	 * @param activity
+	 * @param loadingMessage
+	 */
+	public DefaultTaskListener(Activity activity, String loadingMessage) {
+		this(activity);
+		this.loadingMessage = loadingMessage;
+		this.progressDialog = new ProgressDialog(this.activity);
+		this.progressDialog.setIndeterminate(true);
+	}
 
-    public void onPreExecute() {
-        Log.d(TAG, "onPreExecute");
-        showProgressDialog();
+	public void onPreExecute() {
+		Log.d(TAG, "onPreExecute");
+		showProgressDialog();
 
-    }
+	}
 
-    public void doSuccess(Result result) {
-        Log.d(TAG, "doSuccess");
-        dismissProgressDialog();
-        onSuccess(result);
-    }
+	public void doSuccess(Result result) {
+		Log.d(TAG, "doSuccess");
+		dismissProgressDialog();
+		onSuccess(result);
+	}
 
-    public abstract void onSuccess(Result result);
+	public abstract void onSuccess(Result result);
 
-    public void onProgressUpdate(Integer... prgrss) {
-        Log.d(TAG, "onProgressUpdate");
-    }
+	public void onProgressUpdate(Integer... prgrss) {
+		Log.d(TAG, "onProgressUpdate");
+	}
 
-    public void onCancelled(Object result) {
-        dismissProgressDialog();
-        Log.d(TAG, "onCancelled:result");
-    }
+	public void onCancelled(Result result) {
+		dismissProgressDialog();
+		Log.d(TAG, "onCancelled:result");
+	}
 
-    public void onCancelled() {
-        dismissProgressDialog();
-        Log.d(TAG, "onCancelled");
-    }
+	public void onCancelled() {
+		dismissProgressDialog();
+		Log.d(TAG, "onCancelled");
+	}
 
-    public void onError(Throwable ex) {
-        Log.d(TAG, "onError:" + ex.getClass().getName());
-        dismissProgressDialog();
+	public void onError(Throwable ex) {
+		Log.d(TAG, "onError:" + ex.getClass().getName());
+		dismissProgressDialog();
 
-        //应用异常 界面层
-        if (ex instanceof AppException) {
-            AppException e = (AppException) ex;
-            showError(e.getMessage());
-            return;
-        }
+		//应用异常 界面层
+		if (ex instanceof AppException) {
+			AppException e = (AppException) ex;
+			showError(e.getMessage());
+			return;
+		}
 
-        //超时
-        if (ex instanceof ConnectTimeoutException) {
-            ConnectTimeoutException e = (ConnectTimeoutException) ex;
-            showError("请求超时，请检查网络后重试");
-            return;
-        }
+		//超时
+		if (ex instanceof ConnectTimeoutException) {
+			ConnectTimeoutException e = (ConnectTimeoutException) ex;
+			showError("请求超时，请检查网络后重试");
+			return;
+		}
 
-        //API异常 任务层
-        if (ex instanceof APIException) {
-            APIException e = (APIException) ex;
-            if (null == e.getMessage() || "".equals(e.getMessage())) {
-                showError("未知异常");
-            } else {
-                showError(e.getMessage());
-            }
+		//API异常 任务层
+		if (ex instanceof APIException) {
+			APIException e = (APIException) ex;
+			if (null == e.getMessage() || "".equals(e.getMessage())) {
+				showError("未知异常");
+			} else {
+				showError(e.getMessage());
+			}
 
-            return;
-        }
+			return;
+		}
 
-        //HTTP客户端异常400
-        if (ex instanceof HttpClientErrorException) {
-            HttpClientErrorException e = (HttpClientErrorException) ex;
-            if (null == e.getMessage() || "".equals(e.getMessage())) {
-                showError(e.getStatusText());
-            } else {
-                showError(e.getMessage());
-            }
-            return;
-        }
+		//HTTP客户端异常400
+		if (ex instanceof HttpClientErrorException) {
+			HttpClientErrorException e = (HttpClientErrorException) ex;
+			if (null == e.getMessage() || "".equals(e.getMessage())) {
+				showError(e.getStatusText());
+			} else {
+				showError(e.getMessage());
+			}
+			return;
+		}
 
-        //HTTP服务端异常500
-        if (ex instanceof HttpServerErrorException) {
-            HttpServerErrorException e = (HttpServerErrorException) ex;
-            showError(e.getResponseBodyAsString());
-            return;
-        }
+		//HTTP服务端异常500
+		if (ex instanceof HttpServerErrorException) {
+			HttpServerErrorException e = (HttpServerErrorException) ex;
+			showError(e.getResponseBodyAsString());
+			return;
+		}
 
-        //HTTP资源访问（IO）异常
-        if (ex instanceof ResourceAccessException) {
-            ResourceAccessException e = (ResourceAccessException) ex;
-            showError("无法连接到服务器");
-            return;
-        }
+		//HTTP资源访问（IO）异常
+		if (ex instanceof ResourceAccessException) {
+			ResourceAccessException e = (ResourceAccessException) ex;
+			showError("无法连接到服务器");
+			return;
+		}
 
-        //HTTP未知异常
-        if (ex instanceof RestClientException) {
-            RestClientException e = (RestClientException) ex;
-            showError("未知HTTP异常");
-            return;
-        }
+		//HTTP未知异常
+		if (ex instanceof RestClientException) {
+			RestClientException e = (RestClientException) ex;
+			showError("未知HTTP异常");
+			return;
+		}
 
-        showError("未知任务异常");
+		showError("未知任务异常");
 
-    }
+	}
 
-    //显示进度框
-    private void showProgressDialog() {
-        Log.d(TAG, "showProgressDialog");
-        if (getLoadingMessage() != null) {
-            this.progressDialog.setMessage(getLoadingMessage());
-            this.progressDialog.show();
-        }
-    }
+	//显示进度框
+	private void showProgressDialog() {
+		Log.d(TAG, "showProgressDialog");
+		if (getLoadingMessage() != null) {
+			this.progressDialog.setMessage(getLoadingMessage());
+			this.progressDialog.show();
+		}
+	}
 
-    //隐藏进度框
-    private void dismissProgressDialog() {
-        Log.d(TAG, "dismissProgressDialog");
-        if (getLoadingMessage() != null) {
-            this.progressDialog.dismiss();
-        }
-    }
+	//隐藏进度框
+	private void dismissProgressDialog() {
+		Log.d(TAG, "dismissProgressDialog");
+		if (getLoadingMessage() != null) {
+			this.progressDialog.dismiss();
+		}
+	}
 
-    //显示错误信息
-    private void showError(final String msg) {
-        Log.d(TAG, "showError");
-        Toast.makeText(activity, msg, Toast.LENGTH_LONG).show();
-    }
+	//显示错误信息
+	private void showError(final String msg) {
+		Log.d(TAG, "showError");
+		Toast.makeText(activity, msg, Toast.LENGTH_LONG).show();
+	}
 
-    /**
-     * @return the loadingMessage
-     */
-    public String getLoadingMessage() {
-        return loadingMessage;
-    }
+	/**
+	 * @return the loadingMessage
+	 */
+	public String getLoadingMessage() {
+		return loadingMessage;
+	}
 
 }
