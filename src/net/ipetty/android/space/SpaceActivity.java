f40dec3faad1ce6067aed796cdf6715e1bf0ae00
kongchun <kcrens@gmail.com>
2014-07-01 11:16:29 +0800

    重新构建space list

--- src/net/ipetty/android/space/SpaceActivity.java
+++ src/net/ipetty/android/space/SpaceActivity.java
@@ -1,20 +1,8 @@
 package net.ipetty.android.space;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.ViewFlipper;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
 import java.text.SimpleDateFormat;
 import java.util.List;
+
 import net.ipetty.R;
 import net.ipetty.android.api.UserApiWithCache;
 import net.ipetty.android.bonuspoint.BonusPointActivity;
@@ -41,8 +29,25 @@ import net.ipetty.vo.OptionGroup;
 import net.ipetty.vo.PetVO;
 import net.ipetty.vo.UserStatisticsVO;
 import net.ipetty.vo.UserVO;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.CollectionUtils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class SpaceActivity extends Activity {
 
@@ -68,10 +73,10 @@ public class SpaceActivity extends Activity {
 	private TextView fan_num_text;
 	private TextView action_bar_right_text;// 关注/修改个人信息
 	private View follows;
-	private ImageView avatar;//头像
-	private Boolean isFollow;//记录当前用户是否已关注被查看用户
-	private Integer pageNumber = 0;//我的feed
-	private final Integer pageSize = 5;//我的feed
+	private ImageView avatar;// 头像
+	private Boolean isFollow;// 记录当前用户是否已关注被查看用户
+	private Integer pageNumber = 0;// 我的feed
+	private final Integer pageSize = 5;// 我的feed
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -178,7 +183,7 @@ public class SpaceActivity extends Activity {
 			}
 		});
 
-		//Tab索引
+		// Tab索引
 		viewFlipper = (ViewFlipper) this.findViewById(R.id.viewFlipper);
 		viewFlipper.setDisplayedChild(0);
 
@@ -200,15 +205,17 @@ public class SpaceActivity extends Activity {
 		space_photo_grid = (GridView) space_photo_layout.findViewById(R.id.gridview);
 		space_photo_grid_adapter = new DiscoverAdapter(this);
 		space_photo_grid.setAdapter(space_photo_grid_adapter);
-		new ListByTimelineForHomePage(this)
-				.setListener(new DefaultTaskListener<List<FeedVO>>(SpaceActivity.this) {
-					@Override
-					public void onSuccess(List<FeedVO> result) {
-						space_photo_grid_adapter.loadDate(result);
-					}
-				})
-				.execute(String.valueOf(System.currentTimeMillis()), "0", pageSize.toString());
+		new ListByTimelineForHomePage(this).setListener(new DefaultTaskListener<List<FeedVO>>(SpaceActivity.this) {
+			@Override
+			public void onSuccess(List<FeedVO> result) {
+				space_photo_grid_adapter.loadDate(result);
+			}
+		}).execute(String.valueOf(System.currentTimeMillis()), "0", pageSize.toString());
 
+		// 列表
+		View space_feed_layout = this.findViewById(R.id.space_feed_layout);
+		ListView space_feed_list = (ListView) space_feed_layout.findViewById(R.id.space_feed_list);
+		// TODO: list
 	}
 
 	public class TabClickListener implements OnClickListener {
@@ -234,35 +241,33 @@ public class SpaceActivity extends Activity {
 		}
 	};
 
-	//关注与反关注动作
+	// 关注与反关注动作
 	private OnClickListener toggleFollowClick = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			if (SpaceActivity.this.isFollow) {
-				new Unfollow(SpaceActivity.this)
-						.setListener(new DefaultTaskListener<Boolean>(SpaceActivity.this, "正在反关注...") {
+				new Unfollow(SpaceActivity.this).setListener(
+						new DefaultTaskListener<Boolean>(SpaceActivity.this, "正在反关注...") {
 							@Override
 							public void onSuccess(Boolean result) {
 								SpaceActivity.this.isFollow = false;
 								refreshData();
 							}
-						})
-						.execute(SpaceActivity.this.userId);
+						}).execute(SpaceActivity.this.userId);
 			} else {
-				new Follow(SpaceActivity.this)
-						.setListener(new DefaultTaskListener<Boolean>(SpaceActivity.this, "正在关注...") {
+				new Follow(SpaceActivity.this).setListener(
+						new DefaultTaskListener<Boolean>(SpaceActivity.this, "正在关注...") {
 							@Override
 							public void onSuccess(Boolean result) {
 								SpaceActivity.this.isFollow = true;
 								refreshData();
 							}
-						})
-						.execute(SpaceActivity.this.userId);
+						}).execute(SpaceActivity.this.userId);
 			}
 		}
 	};
 
-	//加载宠物信息
+	// 加载宠物信息
 	private void loadPetsData() {
 		new ListPetsByUserId(SpaceActivity.this).setListener(new DefaultTaskListener<List<PetVO>>(SpaceActivity.this) {
 			@Override
@@ -293,7 +298,7 @@ public class SpaceActivity extends Activity {
 				if (StringUtils.isNotBlank(pet.getGender())) {
 					new GetOptionValueLabelMap(SpaceActivity.this).setListener(
 							new SetOptionLabelTaskListener(SpaceActivity.this, petGender, pet.getGender())).execute(
-									OptionGroup.PET_GENDER);
+							OptionGroup.PET_GENDER);
 				}
 
 				TextView petBirthday = (TextView) space_petty_view.findViewById(R.id.pet_birthday);
@@ -306,7 +311,7 @@ public class SpaceActivity extends Activity {
 				if (StringUtils.isNotBlank(pet.getFamily())) {
 					new GetOptionValueLabelMap(SpaceActivity.this).setListener(
 							new SetOptionLabelTaskListener(SpaceActivity.this, petFamily, pet.getFamily())).execute(
-									OptionGroup.PET_FAMILY);
+							OptionGroup.PET_FAMILY);
 				}
 
 				if (isCurrentUser) {
@@ -334,11 +339,11 @@ public class SpaceActivity extends Activity {
 
 	private void refreshData() {
 		UserVO user = UserApiWithCache.getUserById4Synchronous(this, userId);
-		//头像
+		// 头像
 		if (!StringUtils.isEmpty(user.getAvatar())) {
 			ImageLoader.getInstance().displayImage(Constant.FILE_SERVER_BASE + user.getAvatar(), avatar, options);
 		}
-		//统计数据
+		// 统计数据
 		new GetUserStatisticsByUserId(this).setListener(new DefaultTaskListener<UserStatisticsVO>(SpaceActivity.this) {
 			@Override
 			public void onSuccess(UserStatisticsVO userStatistics) {
@@ -348,10 +353,10 @@ public class SpaceActivity extends Activity {
 			}
 		}).execute(this.userId);
 
-		//宠物信息
+		// 宠物信息
 		loadPetsData();
 
-		//关注/反关注图标
+		// 关注/反关注图标
 		if (!isCurrentUser) {
 			// 判断用户是否关注
 			new IsFollow(this).setListener(new DefaultTaskListener<Boolean>(SpaceActivity.this) {
@@ -367,9 +372,9 @@ public class SpaceActivity extends Activity {
 			}).execute(this.userId);
 		}
 
-		//9宫格
+		// 9宫格
 		space_photo_grid_adapter.notifyDataSetChanged();
-		//feed列表
+		// feed列表
 	}
 
 }
