3fef82d666e2f5b67fa246ef6bbba20cd8462432
jinghai <jinghai.xiao@gmail.com>
2014-07-03 18:34:22 +0800

    isNoneBlank改为-->isNotBlank

--- src/net/ipetty/android/feed/SimpleFeedActivity.java
+++ src/net/ipetty/android/feed/SimpleFeedActivity.java
@@ -1,8 +1,26 @@
 package net.ipetty.android.feed;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import java.util.ArrayList;
 import java.util.List;
-
 import net.ipetty.R;
 import net.ipetty.android.api.UserApiWithCache;
 import net.ipetty.android.comment.CommentActivity;
@@ -26,29 +44,7 @@ import net.ipetty.vo.CommentVO;
 import net.ipetty.vo.FeedFavorVO;
 import net.ipetty.vo.FeedVO;
 import net.ipetty.vo.UserVO;
-
 import org.apache.commons.lang3.StringUtils;
-
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class SimpleFeedActivity extends BaseActivity {
 
@@ -244,7 +240,7 @@ public class SimpleFeedActivity extends BaseActivity {
 
 		Log.d(TAG, "imgURL" + Constant.FILE_SERVER_BASE + feed.getImageSmallURL());
 		// 图片显示
-		if (StringUtils.isNoneBlank(feed.getImageSmallURL())) {
+		if (StringUtils.isNotBlank(feed.getImageSmallURL())) {
 			ImageLoader.getInstance().displayImage(Constant.FILE_SERVER_BASE + feed.getImageSmallURL(), content_image,
 					options);
 		}
