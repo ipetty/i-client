d9ae51028dc23847422d1f23c9d4e0e1c484672c
jinghai <jinghai.xiao@gmail.com>
2014-07-25 17:13:33 +0800

    Revert "解决注销后退的问题"
    This reverts commit 7ce89dd7249270a7769a35f145566d28c45d5856.

--- src/net/ipetty/android/setting/SettingActivity.java
+++ src/net/ipetty/android/setting/SettingActivity.java
@@ -1,5 +1,6 @@
 package net.ipetty.android.setting;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +14,6 @@ import net.ipetty.R;
 import net.ipetty.android.api.UserApiWithCache;
 import net.ipetty.android.core.Constant;
 import net.ipetty.android.core.ui.BackClickListener;
-import net.ipetty.android.core.ui.BaseActivity;
 import net.ipetty.android.core.util.AppUtils;
 import net.ipetty.android.feedback.FeedbackActivity;
 import net.ipetty.android.sdk.core.IpetApi;
@@ -22,7 +22,7 @@ import net.ipetty.android.user.UserActivity;
 import net.ipetty.vo.UserVO;
 import org.apache.commons.lang3.StringUtils;
 
-public class SettingActivity extends BaseActivity {
+public class SettingActivity extends Activity {
 
 	private Button logout;
 	private DisplayImageOptions options = AppUtils.getCacheImageBublder()
