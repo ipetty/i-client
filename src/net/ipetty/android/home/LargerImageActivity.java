92a703fcf3ec0b1f6c61e680f04128dcfb3b8b05
jinghai <jinghai.xiao@gmail.com>
2014-07-30 14:11:41 +0800

    使用PhotoView实现手势放大效果

--- src/net/ipetty/android/home/LargerImageActivity.java
+++ src/net/ipetty/android/home/LargerImageActivity.java
@@ -1,21 +1,27 @@
 package net.ipetty.android.home;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import net.ipetty.R;
 import net.ipetty.android.core.Constant;
 import net.ipetty.android.core.ui.BaseActivity;
 import net.ipetty.android.core.util.AppUtils;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class LargerImageActivity extends BaseActivity {
 
 	private String original_url;
 	private String small_url;
 	private ImageView image;
+	private PhotoViewAttacher mAttacher;
 
 	private DisplayImageOptions options = AppUtils.getNormalImageOptions();
 
@@ -29,8 +35,28 @@ public class LargerImageActivity extends BaseActivity {
 		image = (ImageView) this.findViewById(R.id.image);
 		Uri uri = Uri.fromFile(ImageLoader.getInstance().getDiskCache().get(small_url));
 		image.setImageURI(uri);
-		ImageLoader.getInstance().displayImage(original_url, image, options);
+		ImageLoader.getInstance().displayImage(original_url, image, options, new LoadListener());
 
-		image.setOnTouchListener(new MulitPointTouchListener());// 为图片设置监听
+		//image.setOnTouchListener(new MulitPointTouchListener());// 为图片设置监听
+	}
+
+	private class LoadListener implements ImageLoadingListener {
+
+		public void onLoadingStarted(String imageUri, View view) {
+
+		}
+
+		public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+		}
+
+		public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+			mAttacher = new PhotoViewAttacher(image);
+		}
+
+		public void onLoadingCancelled(String imageUri, View view) {
+
+		}
+
 	}
 }
