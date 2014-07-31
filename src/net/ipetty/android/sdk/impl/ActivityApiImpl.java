3f573db60ec1401d1d7b168fbf334fa975a0a23b
luocanfeng <luocanfeng@gmail.com>
2014-07-31 11:32:04 +0800

    事件流VO与API，回复的回复

--- src/net/ipetty/android/sdk/impl/ActivityApiImpl.java
+++ src/net/ipetty/android/sdk/impl/ActivityApiImpl.java
@@ -1,54 +1,94 @@
 package net.ipetty.android.sdk.impl;
 
-import android.content.Context;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+
+import net.ipetty.android.core.Constant;
 import net.ipetty.android.sdk.core.ApiBase;
 import net.ipetty.sdk.ActivityApi;
 import net.ipetty.vo.ActivityVO;
+
 import org.springframework.util.LinkedMultiValueMap;
+
+import android.content.Context;
 
 /**
  * ActivityApiImpl
- *
+ * 
  * @author luocanfeng
  * @date 2014年6月10日
  */
 public class ActivityApiImpl extends ApiBase implements ActivityApi {
 
-    public ActivityApiImpl(Context context) {
-        super(context);
-    }
+	public ActivityApiImpl(Context context) {
+		super(context);
+	}
 
-    private static final String URI_LIST_ACTIVITIES = "/activity/mine";
+	private static final String URI_LIST_ACTIVITIES = "/activity/mine";
 
-    /**
-     * 获取我的事件流
-     */
-    public List<ActivityVO> listActivities(int pageNumber, int pageSize) {
-        super.requireAuthorization();
+	/**
+	 * 获取我的事件流
+	 */
+	public List<ActivityVO> listActivities(int pageNumber, int pageSize) {
+		super.requireAuthorization();
 
-        LinkedMultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
-        request.add("pageNumber", String.valueOf(pageNumber));
-        request.add("pageSize", String.valueOf(pageSize));
-        URI uri = buildUri(URI_LIST_ACTIVITIES, request);
-        return Arrays.asList(getRestTemplate().getForObject(uri, ActivityVO[].class));
-    }
+		LinkedMultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
+		request.add("pageNumber", String.valueOf(pageNumber));
+		request.add("pageSize", String.valueOf(pageSize));
+		URI uri = buildUri(URI_LIST_ACTIVITIES, request);
+		return Arrays.asList(getRestTemplate().getForObject(uri, ActivityVO[].class));
+	}
 
-    private static final String URI_LIST_RELATED_ACTIVITIES = "/activity/related";
+	private static final String URI_LIST_RELATED_ACTIVITIES = "/activity/related";
 
-    /**
-     * 获取某人相关的事件流
-     */
-    public List<ActivityVO> listRelatedActivities(int pageNumber, int pageSize) {
-        super.requireAuthorization();
+	/**
+	 * 获取某人相关的事件流
+	 */
+	public List<ActivityVO> listRelatedActivities(int pageNumber, int pageSize) {
+		super.requireAuthorization();
 
-        LinkedMultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
-        request.add("pageNumber", String.valueOf(pageNumber));
-        request.add("pageSize", String.valueOf(pageSize));
-        URI uri = buildUri(URI_LIST_RELATED_ACTIVITIES, request);
-        return Arrays.asList(getRestTemplate().getForObject(uri, ActivityVO[].class));
-    }
+		LinkedMultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
+		request.add("pageNumber", String.valueOf(pageNumber));
+		request.add("pageSize", String.valueOf(pageSize));
+		URI uri = buildUri(URI_LIST_RELATED_ACTIVITIES, request);
+		return Arrays.asList(getRestTemplate().getForObject(uri, ActivityVO[].class));
+	}
+
+	private static final String URI_LIST_NEW_FANS_ACTIVITIES = "/activity/newfans";
+
+	/**
+	 * 获取新粉丝事件列表
+	 */
+	public List<ActivityVO> listNewFansActivities() {
+		super.requireAuthorization();
+
+		return Arrays.asList(getRestTemplate().getForObject(Constant.API_SERVER_BASE + URI_LIST_NEW_FANS_ACTIVITIES,
+				ActivityVO[].class));
+	}
+
+	private static final String URI_LIST_NEW_REPLIES_ACTIVITIES = "/activity/newreplies";
+
+	/**
+	 * 获取新回复事件列表
+	 */
+	public List<ActivityVO> listNewRepliesActivities() {
+		super.requireAuthorization();
+
+		return Arrays.asList(getRestTemplate().getForObject(Constant.API_SERVER_BASE + URI_LIST_NEW_REPLIES_ACTIVITIES,
+				ActivityVO[].class));
+	}
+
+	private static final String URI_LIST_NEW_FAVORS_ACTIVITIES = "/activity/newfavors";
+
+	/**
+	 * 获取新赞事件列表
+	 */
+	public List<ActivityVO> listNewFavorsActivities() {
+		super.requireAuthorization();
+
+		return Arrays.asList(getRestTemplate().getForObject(Constant.API_SERVER_BASE + URI_LIST_NEW_FAVORS_ACTIVITIES,
+				ActivityVO[].class));
+	}
 
 }
