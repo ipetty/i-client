248c80dd34b85b945e646d0cbb3ccf0b40e2be87
jinghai <jinghai.xiao@gmail.com>
2014-08-01 14:10:36 +0800

    查看大图增加进度渐变

--- src/net/ipetty/android/home/LargerImageActivity.java
+++ src/net/ipetty/android/home/LargerImageActivity.java
@@ -2,6 +2,7 @@ package net.ipetty.android.home;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,7 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingProgressListener;
 import net.ipetty.R;
 import net.ipetty.android.core.Constant;
 import net.ipetty.android.core.ui.BaseActivity;
@@ -23,11 +25,14 @@ public class LargerImageActivity extends BaseActivity {
 	private String original_url;
 	private String small_url;
 	private ImageView image;
+	//手势放大组件
 	private PhotoViewAttacher mAttacher;
-
 	private DisplayImageOptions options = AppUtils.getNormalImageOptions();
+	//加载大图前默认透明度
+	private int initAlpha = 155;
 
 	@Override
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_larger_image);
@@ -38,36 +43,54 @@ public class LargerImageActivity extends BaseActivity {
 		image = (ImageView) this.findViewById(R.id.image);
 		//设置初始图片
 		image.setScaleType(ImageView.ScaleType.FIT_CENTER);
-		image.setImageResource(R.drawable.default_image);
+		image.setImageAlpha(initAlpha);
+		//image.setImageResource(R.drawable.default_image);
+
+		//同步加载小图
+		image = (ImageView) this.findViewById(R.id.image);
+		Uri uri = Uri.fromFile(ImageLoader.getInstance().getDiskCache().get(small_url));
+		image.setImageURI(uri);
 	}
 
 	//加载数据
 	@Override
 	protected void onViewReady(Bundle savedInstanceState) {
 		Log.d(TAG, "onViewReady");
-		//同步加载小图
-		Bitmap smallImage = ImageLoader.getInstance().loadImageSync(small_url, options);
-		image.setImageBitmap(smallImage);
-		//同步加载大图
-		ImageLoader.getInstance().displayImage(original_url, image, options, new LoadListener());
+
+		//异步加载大图
+		ImageLoader.getInstance().displayImage(original_url, image, options, new LoadListener(), new progressListener());
+	}
+
+	private class progressListener implements ImageLoadingProgressListener {
+
+		public void onProgressUpdate(String imageUri, View view, int current, int total) {
+			Log.d(TAG, "current=" + current + ",total=" + total + ",persent:" + current / total);
+			int persent = (current / total) * 100;
+			if (persent > initAlpha) {
+				image.setImageAlpha(255 * persent / 100);
+			}
+			Log.d(TAG, "当前Alpha:" + 255 * persent);
+		}
 	}
 
 	private class LoadListener implements ImageLoadingListener {
 
 		public void onLoadingStarted(String imageUri, View view) {
-
+			Log.d(TAG, "onLoadingStarted:");
 		}
 
 		public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-
+			Log.d(TAG, "onLoadingFailed:");
 		}
 
 		public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+			Log.d(TAG, "onLoadingComplete:");
 			mAttacher = new PhotoViewAttacher(image);
+			image.setImageAlpha(255);
 		}
 
 		public void onLoadingCancelled(String imageUri, View view) {
-
+			Log.d(TAG, "onLoadingCancelled:");
 		}
 
 	}
